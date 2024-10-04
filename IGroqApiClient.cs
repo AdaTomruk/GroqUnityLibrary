43af6ed1249fe03a41de1410679b966b19ea8956
Ada Tomruk <51528200+AdaTomruk@users.noreply.github.com>
2024-10-04 20:21:23 +0300

    Add files via upload

---
+++ IGroqApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace GroqApiLibrary
+{
+    public interface IGroqApiClient
+    {
+        Task<JObject?> CreateChatCompletionAsync(JObject request);
+        IAsyncEnumerable<JObject?> CreateChatCompletionStreamAsync(JObject request);
+    }
+}
